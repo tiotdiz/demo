55c4c4314843e9f6d756784530faafe90c3c1fb5
tiotdiz <tiotdiz@outlook.com>
2019-10-20 19:49:46 +0800

    Test4First

--- src/demo/HW.java
+++ src/demo/HW.java
@@ -1,7 +1,10 @@
 package demo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class HW {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		System.out.println("Hello Git");
 		System.out.println("Modified");
 	}
